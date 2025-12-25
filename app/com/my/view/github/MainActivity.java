51395af4784874c9b75a1ba59d18bdbff5983ca8
chagasbezerra091-code <chagasbezerra091@gmail.com>
2025-12-24 21:12:39 -0300

    Create MainActivity.java

---
+++ app/com/my/view/github/MainActivity.java
@@ -0,0 +1,30 @@
+/** Copyright (c) 2025 Chagas LLC
+
+All Rights Reserved.
+
+This software is proprietary and confidential.
+Unauthorized copying, modification, distribution,
+or use of this software is strictly prohibited.
+*/
+import android.view.View;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.view.RenderNode;
+import android.view.DisplayListCanvas;
+import android.view.ThreadedRenderer;
+import android.view.ViewParent;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
+import android.view.Choreographer;
+package com.my.view.github;
+class view {
+class IsVisible {
+boolean mIsVisible;
+view.setVisibility(View.VISIBLE);
+if view.visibility(View.Yes);
+if view.visibility(View.No);
+}
+view.emoji(View.EMOJI);
+}
